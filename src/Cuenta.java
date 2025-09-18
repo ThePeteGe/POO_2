28214cded02685a7015520d1ddfe2eb23c43e6f0
Nestor <nestorgilgil6366@gmail.com>
2025-09-18 17:54:30 -0500

    POO 2

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class Cuenta {
+
+    private int numerodecuenta;
+    private int cedula;
+    private Double saldoactual;
+
+    //constructor vacio
+    public Cuenta () {
+    }
+    //CONSTRUCTOR CON PARAMETROS
+
+
+    public Cuenta(int numerodecuenta, int cedula, Double saldoactual) {
+        this.numerodecuenta = numerodecuenta;
+        this.cedula = cedula;
+        this.saldoactual = saldoactual;
+    }
+    //get and set
+
+
+    public int getNumerodecuenta() {
+        return numerodecuenta;
+    }
+
+    public void setNumerodecuenta(int numerodecuenta) {
+        this.numerodecuenta = numerodecuenta;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public Double getSaldoactual() {
+        return saldoactual;
+    }
+
+    public void setSaldoactual(Double saldoactual) {
+        this.saldoactual = saldoactual;
+    }
+    //metodo tostring
+
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "numerodecuenta=" + numerodecuenta +
+                ", cedula=" + cedula +
+                ", saldoactual=" + saldoactual +
+                '}';
+    }
+
 }
